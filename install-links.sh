cf68fd358dd9c2a56784a342145c9c08a52ba7f3
Aaron Schaefer <aaron@elasticdog.com>
2009-03-18 22:59:01 -0400

    Add a dotfile symlink installation script

---
+++ install-links.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Create needed directories if they don't already exist
+mkdir -p "$HOME/.ssh"
+
+FILES="
+bash_profile
+bashrc
+factor-rc
+gitconfig
+gitignore
+hgrc
+irbrc
+screenrc
+ssh/config
+vim
+vimrc
+zshenv
+zshrc
+"
+
+# Create symbolic links for all configuration files
+for file in $FILES
+do
+	SOURCE="$HOME/.dotfiles/$file"
+	TARGET="$HOME/.$file"
+
+	# Create backup file if the target already exists and is not a symlink
+	if [ -f "$TARGET" ] && [ ! -L "$TARGET" ]
+	then
+		echo "$TARGET exists; copying to $TARGET.bak"
+		cp -p "$TARGET" "$TARGET.bak"
+	fi
+	ln -fsv "$SOURCE" "$TARGET"
+done
+
+exit 0
