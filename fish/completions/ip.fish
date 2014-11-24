2d5b747b390d798befd2c94df89682663b22d537
Aaron Bull Schaefer <aaron@elasticdog.com>
2014-11-24 10:53:31 -0800

    Add zenbear to ip calculation function

--- fish/completions/ip.fish
+++ fish/completions/ip.fish
@@ -1,1 +1,1 @@
-complete -c ip -x -a (echo api-{stage,prod}-api-server api-prod-{consul-server,consul-webui,elasticsearch,jenkins-master,kibana,logstash}) --description 'Instance name'
+complete -c ip -x -a (echo api-{stage,prod}-api-server api-prod-{consul-server,consul-webui,elasticsearch,jenkins-master,kibana,logstash,zenbear}) --description 'Instance name'
