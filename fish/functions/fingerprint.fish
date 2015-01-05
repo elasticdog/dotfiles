26946bc983242ed39a2398b3764bc4fac23d5b7d
Aaron Bull Schaefer <aaron@elasticdog.com>
2015-01-05 10:37:54 -0800

    Add a function to generate AWS-formatted key fingerprints

---
+++ fish/functions/fingerprint.fish
@@ -0,0 +1,10 @@
+function fingerprint --description 'Generate an AWS key pair fingerprint'
+
+	if set -q $argv
+		echo 'Usage: fingerprint <pubkey>'
+		echo 'Generate the fingerprint of an SSH public key that matches the format used by AWS'
+	else
+		command ssh-keygen -f "$argv[1]" -e -m PKCS8 | command openssl pkey -pubin -outform DER | command openssl md5 -c
+	end
+
+end
