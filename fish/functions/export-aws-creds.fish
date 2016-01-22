5c9334309e2d1f0705c38116c45c5905eef81489
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-01-22 10:26:07 -0800

    Ensure the AWS credential vars are global
    Not sure how this was working previously.

--- fish/functions/export-aws-creds.fish
+++ fish/functions/export-aws-creds.fish
@@ -6,10 +6,10 @@ function export-aws-creds --description 'Export AWS credentials into the shell e
 	else
 		set -l AWS_CREDENTIALS_FILE $HOME/.aws/credentials
 		if test -f $AWS_CREDENTIALS_FILE
-			set -x AWS_ACCESS_KEY (ini-section $argv[1] $AWS_CREDENTIALS_FILE | awk '/^aws_access_key_id/{ print $3 }')
-			set -x AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY
-			set -x AWS_SECRET_KEY (ini-section $argv[1] $AWS_CREDENTIALS_FILE | awk '/^aws_secret_access_key/{ print $3 }')
-			set -x AWS_SECRET_ACCESS_KEY $AWS_SECRET_KEY
+			set -xg AWS_ACCESS_KEY (ini-section $argv[1] $AWS_CREDENTIALS_FILE | awk '/^aws_access_key_id/{ print $3 }')
+			set -xg AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY
+			set -xg AWS_SECRET_KEY (ini-section $argv[1] $AWS_CREDENTIALS_FILE | awk '/^aws_secret_access_key/{ print $3 }')
+			set -xg AWS_SECRET_ACCESS_KEY $AWS_SECRET_KEY
 		end
 	end
 
