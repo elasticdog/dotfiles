a3e51558a651bd5dc5d9a835a07f0e931090bb42
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-02-12 13:39:59 -0800

    Add a hyper binding to focus on the Slack application

--- hammerspoon/applications.lua
+++ hammerspoon/applications.lua
@@ -6,6 +6,7 @@ local minimash = {"cmd", "alt"}
 -- launch applications
 hs.hotkey.bind(hyper, "F", function () hs.application.launchOrFocus("Firefox") end)
 hs.hotkey.bind(hyper, "N", function () hs.application.launchOrFocus("nvALT") end)
+hs.hotkey.bind(hyper, "S", function () hs.application.launchOrFocus("Slack") end)
 
 -- manually reload the hammerspoon config
 hs.hotkey.bind(hyper, "R", function() hs.reload() end)
