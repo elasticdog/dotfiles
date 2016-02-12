fa8ce69fafe39c13546f0913a843ae067de915f3
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-02-12 14:03:25 -0800

    Add notifications to location-based volume muting
    I've also switched to actually muting/restoring rather than setting the
    absolute volume to 0 and 25 respectively.

--- hammerspoon/location.lua
+++ hammerspoon/location.lua
@@ -2,16 +2,24 @@ local wifiWatcher = nil
 local homeSSID = "deadlight"
 local lastSSID = hs.wifi.currentNetwork()
 
--- Mute the volume when not at home
+-- mute the speaker volume when not at home
 function ssidChangedCallback()
     newSSID = hs.wifi.currentNetwork()
 
     if newSSID == homeSSID and lastSSID ~= homeSSID then
-        -- We just joined our home WiFi network
-        hs.audiodevice.defaultOutputDevice():setVolume(25)
+        -- we just joined our home wifi network
+        hs.audiodevice.defaultOutputDevice():setMuted(false)
+        hs.notify.new({
+            title='Hammerspoon',
+            informativeText='Arrived home; speaker volume un-muted'
+        }):send()
     elseif newSSID ~= homeSSID and lastSSID == homeSSID then
-        -- We just departed our home WiFi network
-        hs.audiodevice.defaultOutputDevice():setVolume(0)
+        -- we just joined a wifi network outside of our home
+        hs.audiodevice.defaultOutputDevice():setMuted(true)
+        hs.notify.new({
+            title='Hammerspoon',
+            informativeText='Left home; speaker volume muted'
+        }):send()
     end
 
     lastSSID = newSSID
