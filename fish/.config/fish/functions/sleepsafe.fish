a9204fcdd9b9dd18f59a9ae1555cb0c12a75fe5f
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-12-19 07:35:35 -0800

    Add doc reference regarding pmset to sleepsafe alias

--- fish/.config/fish/functions/sleepsafe.fish
+++ fish/.config/fish/functions/sleepsafe.fish
@@ -1,4 +1,5 @@
 function sleepsafe --description 'Set hibernation mode to be more secure'
+	# https://github.com/drduh/macOS-Security-and-Privacy-Guide#full-disk-encryption
 
 	command sudo pmset -a destroyfvkeyonstandby 1 hibernatemode 25
 
