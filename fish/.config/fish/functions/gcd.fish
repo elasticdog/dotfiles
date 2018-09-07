58e599be01cf372a455d44d91d985a534347935c
Aaron Bull Schaefer <aaron@elasticdog.com>
2018-09-07 13:30:44 -0700

    Allow providing a subdirectory argument to gcd function

--- fish/.config/fish/functions/gcd.fish
+++ fish/.config/fish/functions/gcd.fish
@@ -1,7 +1,7 @@
 function gcd --description 'Change to the top-level directory of the current Git repository'
 
 	if set top_level (command git rev-parse --show-toplevel)
-		cd $top_level
+		cd "$top_level/$argv[1]"
 	else
 		return 1
 	end
