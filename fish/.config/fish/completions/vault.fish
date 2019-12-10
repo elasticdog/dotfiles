3015574b231d85b9f90bdb4c31dee103093f9823
Aaron Bull Schaefer <aaron@elasticdog.com>
2019-12-10 11:01:15 -0800

    Use generic path to vault for auto-completion script
    This way I won't have to update it every time there's a new release.

--- fish/.config/fish/completions/vault.fish
+++ fish/.config/fish/completions/vault.fish
@@ -3,7 +3,7 @@ function __complete_vault
     set -lx COMP_LINE (string join ' ' (commandline -o))
     test (commandline -ct) = ""
     and set COMP_LINE "$COMP_LINE "
-    /nix/store/lv423g56bbild6d3l7x1d2xlm99j19pl-vault-0.9.4/bin/vault
+    ~/.nix-profile/bin/vault
 end
 complete -c vault -a "(__complete_vault)"
 
