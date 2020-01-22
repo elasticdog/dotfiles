8e3670e3d70f8c5e25c66373a20e47615b2a36c1
Aaron Bull Schaefer <aaron@elasticdog.com>
2020-01-22 15:49:49 -0800

    Use $PATH for Vault binary used for completions

--- fish/.config/fish/completions/vault.fish
+++ fish/.config/fish/completions/vault.fish
@@ -3,7 +3,7 @@ function __complete_vault
     set -lx COMP_LINE (string join ' ' (commandline -o))
     test (commandline -ct) = ""
     and set COMP_LINE "$COMP_LINE "
-    ~/.nix-profile/bin/vault
+    vault
 end
 complete -c vault -a "(__complete_vault)"
 
